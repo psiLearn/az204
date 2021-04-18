64d5d05d4deb8449aa0de26e086a06c171f3e49d
Peter Siebke <siebke.peter@googlemail.com>
2021-04-19 01:41:36 +0200

    deploying api.zip from web

--- tryot.fsx
+++ tryot.fsx
@@ -1,3 +1,37 @@
+#r "System.Net.WebClient.dll"
+
+open System
+open System.Net
+open System.IO
+open System.Diagnostics
+
+let loadUp() = 
+    let webAppName  = Environment.GetEnvironmentVariable("TEMP_WEB_APP_NAME", EnvironmentVariableTarget.User)
+    printfn "updating: %A" webAppName
+    let fileName =  Path.GetTempFileName() 
+                    |> fun n -> [   //n |> Path.GetDirectoryName
+                                    n |> Path.GetFileNameWithoutExtension
+                                    ".zip"  ] |> List.reduce (+) 
+    printfn "temp file: %A"  fileName  
+    let url  = @"https://github.com/MicrosoftLearning/AZ-204-DevelopingSolutionsforMicrosoftAzure/blob/master/Allfiles/Labs/01/Starter/API/api.zip?raw=true"
+    let webClient = new WebClient()
+    printfn "starting downloading"  
+    webClient.DownloadFile (Uri url, fileName)
+    // let req = url |> Uri |> WebRequest.Create
+    // use resp = req.GetResponse()
+    // use stream = resp.GetResponseStream()
+    // use writer = new FileStream(fileName, FileMode.Create,  FileAccess.Write)
+    // writer.
+    printfn "starting uploading"  
+    let pars = sprintf "webapp deployment source config-zip --resource-group ManagedPlatform --src %A --name %s" fileName webAppName
+    printfn "az %s" pars  
+    let az = Process.Start("az", pars)
+    az.WaitForExit()
+    File.Delete fileName
+loadUp()
+
+
+
 #r "nuget: Azure.Storage.Blobs"
 
 open System
